f7314351b985d565d5b72237cdfa20a700d19fe9
David Moser <david.moser@gmx.net>
2015-09-07 00:15:58 +0200

    new configuration parameters

--- configuration.rb
+++ configuration.rb
@@ -2,21 +2,32 @@ require 'solar_integration/dhtml_dialog.rb'
 require 'solar_integration/data_collector.rb'
 
 class Configuration < DhtmlDialog
-  attr_accessor :sun_states, :tile_length, :tsi,
-    :infer_square_length_from_face, :square_length, :inclination_cutoff, :sky_section_size,
-    :active_data_collectors, :advanced_options_on, :assume_faces_up
+  attr_accessor :sun_states,
+    :tile_length,
+    :advanced_options_on,
+    :infer_square_length_from_face,
+    :square_length,
+    :inclination_cutoff,
+    :sky_section_size,
+    :assume_faces_up,
+    :global_horizontal_irradiation,
+    :cell_efficiency,
+    :system_losses,
+    :active_data_collectors
   
   def initialize_values
     @skip_variables = ['@active_data_collectors']
     @sun_states = 1000
     @tile_length = 0.3 # m
-    @tsi = 250 # W/m^2
     @advanced_options_on = false
     @infer_square_length_from_face = true
     @square_length = 3 # m
     @inclination_cutoff = 30 # degrees
     @sky_section_size = 5 # degrees
     @assume_faces_up = true
+    @global_horizontal_irradiation = 1200 # kWh / m2, central europe
+    @cell_efficiency = 20 # %
+    @system_losses = 11 # 8% temperature, 3% reflection
     @active_data_collectors = [TotalIrradianceTiles] # not configurable at the moment
   end
   
